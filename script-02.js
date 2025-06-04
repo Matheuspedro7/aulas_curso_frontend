a3c4a9e1038b4ddb18bb96aba11a9b888ac4e240
Matheuspedro7 <matheuspedrogomesleite5@gmail.com>
2025-06-04 19:59:59 -0300

    qualquer coisa

--- script-02.js
+++ script-02.js
@@ -1,6 +1,29 @@
 function soma(){
   var a = document.getElementById('valor01').value;
   var b = document.getElementById('valor02').value;
-  document.getElementById('demo').innerHTML = a + b;
-  //document.getElementById('demo').innerHTML = parseInt(a) + parseInt(b);
+  document.getElementById('demo').innerHTML = parseInt(a) + parseInt(b);
+}
+
+function subtracao(){
+  var a = document.getElementById('valor01').value;
+  var b = document.getElementById('valor02').value;
+  document.getElementById('demo').innerHTML = parseInt(a) - parseInt(b);
+}
+
+function multiplicacao(){
+  var a = document.getElementById('valor01').value;
+  var b = document.getElementById('valor02').value;
+  document.getElementById('demo').innerHTML = parseInt(a) * parseInt(b);
+}
+
+function divisao(){
+  var a = document.getElementById('valor01').value;
+  var b = document.getElementById('valor02').value;
+  document.getElementById('demo').innerHTML = parseInt(a) / parseInt(b);
+}
+
+function potenciacao(){
+  var a = document.getElementById('valor01').value;
+  var b = document.getElementById('valor02').value;
+  document.getElementById('demo').innerHTML = parseInt(a) ** parseInt(b);
 }
