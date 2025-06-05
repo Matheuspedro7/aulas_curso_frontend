cb7c433c6fa5d0321f76387094b756a16b8ba714
Matheuspedro7 <matheuspedrogomesleite5@gmail.com>
2025-06-04 21:27:34 -0300

    aula  java

--- script-03.js
+++ script-03.js
@@ -1,10 +1,17 @@
 function parImpar(){
   var a = parseInt(document.getElementById('valor01').value);
  
-  if(a % 2 == 0){
+  if(!(a % 2 == 1)){
     document.getElementById('demo').innerHTML = "É um número par.";
   }else{
     document.getElementById('demo').innerHTML = "É um número ímpar.";
   }
 
 }
+
+/*if (a % 2 == 1){
+  document.getElementById('demo').innerHTML = "É um número par.";
+}else{
+  document.getElementById('demo').innerHTML = "É um número ímpar.";
+}*/
+
--- script-04.js
+++ script-04.js
@@ -1,12 +1,28 @@
 function media(){
+  //+-----------
+  //|Recebe notas
+  //+-----------
+
   var a = parseFloat(document.getElementById('valor01').value);
   var b = parseFloat(document.getElementById('valor02').value);
-  var m = (a+b)/2;
+  var c = parseFloat(document.getElementById('valor03').value);
+  //+----------
+  //|Calculo média
+  //+----------
+  
+  var m =(a+b+c)/3;
 
-  if(m == 10){
+  //+------------
+  //| Verificação de notas
+  //|Saídas esperadas: (Aprovado com Distinção, Aprovado, Quase, mas reprovado, Reprovado)
+  //+------------
+
+  if (m == 10){
     document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Aprovado com Distinção)";
   } else if (m>=7) {
     document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Aprovado)";
+  }else if (m>=5) {
+    document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Quase, mas reprovado)";
   }else{
     document.getElementById('demo').innerHTML = "Média = "+ m.toString() + "(Reprovado)";
   }
